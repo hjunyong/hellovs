b2b7ba4748f74b27275d4dfe943e940a33bede11
YHBY <1552736664@qq.com>
2021-03-16 16:47:19 +0800

    0.0.2

--- hellovs/hellovs/hellovs.cpp
+++ hellovs/hellovs/hellovs.cpp
@@ -5,6 +5,7 @@ int main()
 {
 	cout << "Hello Visual Studio 2017." << endl;
 	cout << "Hello world version 1.0.0" << endl;
+	cout<< "github desktop examination."<<endl;
 	system("pause");
 	return 0;
 }
