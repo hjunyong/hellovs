ccb698b39305ac8462dfacfe6be195a70136825d
YHBY <1552736664@qq.com>
2021-03-16 19:03:42 +0800

    0.0.3

--- hellovs/hellovs/hellovs.cpp
+++ hellovs/hellovs/hellovs.cpp
@@ -6,6 +6,6 @@ int main()
 	cout << "Hello Visual Studio 2017." << endl;
 	cout << "Hello world version 1.0.0" << endl;
 	cout<< "github desktop examination."<<endl;
-	system("pause");
+	// system("pause");
 	return 0;
 }
